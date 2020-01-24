644c9580077c647c680735bbe645ce1b5abdcd97
Anunay Maheshwari <anunaym14@gmail.com>
2020-01-24 22:32:25 +0530

    DMC: go_back_n: Switch to cumulative acknowledgement and code cleanup
    Signed-off-by: Anunay Maheshwari <anunaym14@gmail.com>

--- Trimester-V/DMC/go_back_n.cpp
+++ Trimester-V/DMC/go_back_n.cpp
@@ -1,29 +1,26 @@
-#include<iostream>
-
-using namespace std;
+#include <iostream>
 
 int main() {
     int frames, window, i = 1;
-    cout << "Enter number of frames to send: ";
-    cin >> frames;
-    cout << "Enter window size: ";
-    cin >> window;
+    
+    std::cout << "Enter number of frames to send: ";
+    std::cin >> frames;
+    std::cout << "Enter window size: ";
+    std::cin >> window;
+    
     while(i <= frames) {
-        int x = 0;
         for(int j = i; j < i + window && j <= frames; j++) {
-            cout << "Sending frame " << j << "\n";
+            std::cout << "Sending frame " << j << "\n";
         }
-        for(int j = i; j < i + window && j <= frames; j++) {
-            string ans;
-            cout << "Did you receive frame " << j << "? ";
-            cin >> ans;
-            if(ans == "Yes") 
-                x++;
-            else {
-                cout << "Retransmitting window\n";
-                break;
-            }
+
+        char ans;
+        std::cout << "Did you receive the window? ";
+        std::cin >> ans;
+    
+        if(ans == 'Y' || ans == 'y') {
+            i += window;
         }
-        i += x;
     }
+    
+    return 0;
 }
